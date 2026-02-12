289242db4505d8ec952fcf2c7d3873d0002f9ddb
valentinsonu <valentin.sonu@gmail.com>
2026-02-12 08:15:07 +0700

    Added connect/disconnect

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,18 @@
             <div class="status-dot"></div>
             <span>{{ connectionStatus }}</span>
             <span v-if="isConnected">• {{ totalDevices }} devices • {{ totalPgns }} PGNs/sentences</span>
+
+            <button v-if="!isConnected" class="btn btn-primary btn-sm"
+                    @click="$emit('connectWebSocket')">
+                <i class="fas fa-plug" :class="isConnecting?'btn-fa-plug-circle-bolt':'fa-plug'"></i> Connect
+            </button>
+            <button v-else-if="isConnecting" class="btn btn-primary btn-sm"
+                    @click="$emit('connectWebSocket')">
+                <i class="fas fa-plug-circle-bolt" :class="isConnecting?'btn-fa-plug-circle-bolt':'fa-plug'"></i> Connecting
+            </button>
+            <button v-if="isConnected" class="btn btn-danger btn-sm" @click="$emit('disconnectWebSocket')">
+                <i class="fas fa-plug-circle-minus"></i> Disconnect
+            </button>
         </div>
     </header>
 </template>
@@ -12,6 +24,7 @@
 <script setup>
 defineProps({
     isConnected:      Boolean,
+    isConnecting:     Boolean,
     connectionStatus: String,
     totalDevices:     Number,
     totalPgns:        Number
